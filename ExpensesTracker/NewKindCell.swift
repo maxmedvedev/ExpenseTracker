4e8f4cafae5b49c757b44f74fbd798561ea7d1b5
Max Medvedev <maxim.medvedev@jetbrains.com>
2017-03-26 20:07:45 +0300

    fix kind index selection

--- ExpensesTracker/NewKindCell.swift
+++ ExpensesTracker/NewKindCell.swift
@@ -16,17 +16,28 @@ class NewKindCell: UITableViewCell {
             return
         }
 
-        let count = viewController.kinds.count
-        let newKind = ExpenseKind(name: text, index: count)
+        let newKind = ExpenseKind(name: text, index: findNewIndex(viewController.kinds))
 
         viewController.kinds.append(newKind)
 
         let table = viewController.tableView!
         table.beginUpdates()
-        table.insertRows(at: [IndexPath(row: count, section: 0)], with: .automatic)
+        table.insertRows(at: [IndexPath(row: viewController.kinds.count, section: 0)], with: .automatic)
         table.endUpdates()
 
         textField.text = ""
         textField.resignFirstResponder()
+    }
+
+    private func findNewIndex(_ kinds: [ExpenseKind]) -> Int {
+        var set = Set<Int>()
+        kinds.forEach { set.insert($0.index) }
+
+        var result = 0
+
+        while true {
+            if !set.contains(result) { return result }
+            result += 1
+        }
     }
 }
